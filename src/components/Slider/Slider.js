017d82b9acf2504d404bdec30e7a227f3949be79
DungNguyenVan02 <nguyenvandung12112002@gmail.com>
2023-07-15 12:46:08 +0700

    fix slider home

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,21 +1,24 @@
-import classNames from 'classnames/bind';
-import styles from './Slider.module.scss';
-import { useSelector } from 'react-redux';
-import { bannerSelector } from '~/redux/selector';
-
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
-
+import { useRef } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Slider.module.scss';
+import { useSelector } from 'react-redux';
+import { bannerSelector } from '~/redux/selector';
+import { Pagination, Navigation, Autoplay, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 
 function Slider() {
     const bannerList = useSelector(bannerSelector);
+
+    const navigationPrevRef = useRef(null);
+    const navigationNextRef = useRef(null);
 
     const render = bannerList?.map((item, index) => {
         return (
@@ -27,16 +30,36 @@ function Slider() {
     return (
         <div className={cx('wrapper')}>
             <Swiper
-                modules={[Navigation, Pagination, Autoplay]}
+                modules={[Navigation, Pagination, Autoplay, A11y]}
                 spaceBetween={25}
                 slidesPerView={3}
-                navigation={true}
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                autoplay={{ delay: 1000 }}
                 loop={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    dynamicBullets: true,
+                }}
+                navigation={{
+                    prevEl: navigationPrevRef.current,
+                    nextEl: navigationNextRef.current,
+                }}
+                onBeforeInit={(swiper) => {
+                    swiper.params.navigation.prevEl = navigationPrevRef.current;
+                    swiper.params.navigation.nextEl = navigationNextRef.current;
+                }}
+                speed={600}
+                allowTouchMove={false}
+                scrollbar={{ draggable: false }}
             >
                 {render}
+                <button ref={navigationPrevRef} className={cx('btn-prev')}>
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+                <button ref={navigationNextRef} className={cx('btn-next')}>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </button>
             </Swiper>
         </div>
     );
